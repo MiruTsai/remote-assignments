1d3f3a57bf85579fcdd662699735285d3c531e69
MiruTsai <zct398@gmail.com>
2019-07-08 19:12:51 +0800

    change import

--- week-4-2/src/index.js
+++ week-4-2/src/index.js
@@ -1,4 +1,3 @@
-import './index.css';
 import logo from './logo.png';
 import exit from './iconfinder_00-ELASTOFONT-STORE-READY_close_2703079.png';
 import menu from './iconfinder_Menu_4473024.png';
