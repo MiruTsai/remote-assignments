6e8f5f8e7ce67538b3d6b90f3f6bea876e54b62e
MiruTsai <zct398@gmail.com>
2019-07-08 19:04:51 +0800

    change import

--- week-4-2/src/index.js
+++ week-4-2/src/index.js
@@ -1,6 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
 import logo from './logo.png'
 import exit from './iconfinder_00-ELASTOFONT-STORE-READY_close_2703079.png'
 import menu from './iconfinder_Menu_4473024.png'
@@ -123,8 +120,6 @@ class App extends React.Component {
                 <Container />                
                 <Containers_2 />                
             </div>
-
-
         );
     }
 }
