6f25a2aa1766a132e089c444f7494e0c4a866e19
MiruTsai <zct398@gmail.com>
2019-07-08 18:47:34 +0800

    week4-2

--- week-4-2/src/index.js
+++ week-4-2/src/index.js
@@ -1,103 +1,130 @@
-const Header =(props) => {
-         return (
-            <header>
-                <h1>{props.title}</h1>
-                <span className="stats">Drinks:{props.totalDrinks}</span>                
-            </header>
-        );
-    }
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import logo from './logo.png'
+import exit from './iconfinder_00-ELASTOFONT-STORE-READY_close_2703079.png'
+import menu from './iconfinder_Menu_4473024.png'
 
-
-const Drink = (props) => {
+const Header = () => {
     return (
-        <div className="drink">
-            <span className="drink-name">
-            <button className="remove-drink" onClick={() =>props.removePlayer(props.id)}>✖</button>
-                {props.name}
-            </span>
-            <Counter />
-        </div>
-    );
+        <header id="TOP" className="main-header">
+            <h1 className="title">SHOW-U</h1>
+            <ul className="main-nav">
+                <li><a href="#">item1</a></li>
+                <li><a href="#">item2</a></li>
+                <li><a href="#">item3</a></li>
+                <li><a href="#">item4</a></li>
+            </ul>
+        <img id="menu" src={menu} onClick={()=>{
+            document.getElementById('menu').style.display = 'none';
+            document.getElementById('exit').style.display = 'inline-block';
+            document.querySelector('.main-nav').style.display = 'flex'}}></img>
+        <img id="exit" src={exit} onClick={()=>{
+            document.getElementById('exit').style.display = 'none';
+            document.getElementById('menu').style.display = 'inline-block';
+            document.querySelector('.main-nav').style.display = 'none'}}></img>
+        <img id="logo" src={logo}></img>
+            
+        </header>
+    )
 }
 
-class Counter extends React.Component {
-    state = {
-        cups: 0
-    };
-
-    incrementCup = () => {
-        this.setState(prevState => {            
-            return {
-                cups: prevState.cups + 1
-            }
-        });
-    }
-
-    decrementCup = () => {
-        this.setState(prevState => {
-            if(prevState.cups > 0)
-            return {
-                cups: prevState.cups - 1
-            };
-        });
-    }
+class Welcome extends React.Component {
     render() {
         return (
-            <div className="counter">
-                <button className="counter-action decrement" onClick={this.decrementCup}>-</button>
-                <span className="counter-score">{this.state.cups}</span>
-                <button className="counter-action increment" onClick={this.incrementCup}>+</button>
+            <div className="welcome">
+                <h1 id="myHeading" onClick={() => document.getElementById("myHeading").textContent = "Have a Good Time!"}>
+                    welcome message
+                </h1>
+            </div>
+        )
+    }
+}
+
+
+
+
+const ContainerTitle = () => {
+    return (
+        <h2 className="containerTitle">Section Title</h2>
+    )
+}
+
+class Container extends React.Component {
+    render() {
+        return (
+            <div className="containers">
+                <div className="first row">
+                    <div>
+                        <p className="content">content one</p>
+                    </div>
+                </div>
+                <div className="second row">
+                    <div>
+                        <p className="content">content two</p>
+                    </div>
+                </div>
+                <div className="third row">
+                    <div>
+                        <p className="content">content three</p>
+                    </div>
+                </div>
+                <div className="fourth row">
+                    <div>
+                        <p className="content">content four</p>
+                    </div>
+                </div>
+                <button id="myButton" onClick={() => document.getElementById('containers_2').style.display = "flex"}>Call more box</button>
             </div>
         );
     }
 }
-class App extends React.Component {
-        state = {
-            drinks:[
-            {
-                name: "Bubble Tea",
-                id: 1
-            },
-            {
-                name: "Lemon Yogurt",
-                id: 2
-            },
-            {
-                name: "Matcha",
-                id: 3
-            },
-            {
-                name: "Chocoalate Milk",
-                id: 4
-            }
-            ]
-        };
 
-        handleRemovePlayer = (id) => {
-            this.setState(prevState=>{
-                return{
-                    drinks:prevState.drinks.filter(p => p.id !== id)
-                };
-                
-            });
 
-        }
 
+class Containers_2 extends React.Component {
     render() {
         return (
-            <div className="orderDrink">
-                <Header title="Choose your summer favorite drink" totalDrinks={this.state.drinks.length} />
+            <div id="containers_2">
+                <div className="first row">
+                    <div>
+                        <p className="content">content one</p>
+                    </div>
+                </div>
+                <div className="second row">
+                    <div>
+                        <p className="content">content two</p>
+                    </div>
+                </div>
+                <div className="third row">
+                    <div>
+                        <p className="content">content three</p>
+                    </div>
+                </div>
+                <div className="fourth row">
+                    <div>
+                        <p className="content">content four</p>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
 
-                
-                {this.state.drinks.map(drink =>
-                    <Drink
-                        name={drink.name}
-                        id={drink.id}
-                        key={drink.id.toString()}
-                        removePlayer = {this.handleRemovePlayer}
-                    />
-                )}                
-            </div>            
+
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Header />
+                <Welcome />
+                <ContainerTitle />
+                <Container />                
+                <Containers_2 />                
+            </div>
+
+
         );
     }
 }
