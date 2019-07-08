027aab9bc4b04e4b67c8516f3c4e504fe18822ed
MiruTsai <zct398@gmail.com>
2019-07-08 14:09:34 +0800

    assignment 2 adj

--- week-4/assignment2.js
+++ week-4/assignment2.js
@@ -1,22 +1,57 @@
-function ajax(src, callback){ 
-    let xhr = new XMLHttpRequest();     
-        xhr.onreadystatechange = function callback(response) {
-            if (xhr.readyState === 4){
-            response = JSON.parse(xhr.responseText);
-            console.log(response);
-            let goodsList = '';
-            for (let i =0 ;i<response.length;i+=1){
-                goodsList += `<div class=goodList${i}>`;
-                goodsList += `<h2 class=name>${response[i].name}</h2>`;
-                goodsList += `<div class=price>$${response[i].price}</div>`;
-                goodsList += `<div class=desc>${response[i].description}</div>`;
-                goodsList += `</div>`;                     
-            }
-            document.getElementById('goods').innerHTML = goodsList;
-    }
-}
+function ajax(src, callback) { 
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+     if (xhr.readyState === 4) {
+      callback(xhr.responseText);
+     }
+    };
     xhr.open('GET',src);
     xhr.send();
-}
-ajax("https://cwpeng.github.io/live-records-samples/data/products.json", 
-function(response){ render(response); });     
+   }
+   
+   function render(response) {
+    let goodList = JSON.parse(response);
+    let html = '';
+    for (let i = 0; i < goodList.length; i += 1) {
+     html += `<div class=goodList${i}>`;
+     html += `<h2 class=name>${goodList[i].name}</h2>`;
+     html += `<div class=price>$${goodList[i].price}</div>`;
+     html += `<div class=desc>${goodList[i].description}</div>`;
+     html += `</div>`;
+    }
+    document.getElementById('goods').innerHTML = html;
+   }
+   
+   ajax("https://cwpeng.github.io/live-records-samples/data/products.json", function(response) {
+    render(response);
+   });
+
+
+
+
+
+
+
+
+// function ajax(src, callback) {
+//     let xhr = new XMLHttpRequest();
+//     xhr.onreadystatechange = function callback(response) {
+//         if (xhr.readyState === 4) {
+//             response = JSON.parse(xhr.responseText);
+//             console.log(response);
+//             let goodsList = '';
+//             for (let i = 0; i < response.length; i += 1) {
+//                 goodsList += `<div class=goodList${i}>`;
+//                 goodsList += `<h2 class=name>${response[i].name}</h2>`;
+//                 goodsList += `<div class=price>$${response[i].price}</div>`;
+//                 goodsList += `<div class=desc>${response[i].description}</div>`;
+//                 goodsList += `</div>`;
+//             }
+//             document.getElementById('goods').innerHTML = goodsList;
+//         }
+//     }
+//     xhr.open('GET', src);
+//     xhr.send();
+// }
+// ajax("https://cwpeng.github.io/live-records-samples/data/products.json",
+//     function (response) { render(response); });     
